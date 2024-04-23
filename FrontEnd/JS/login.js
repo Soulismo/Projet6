0df4276757a9b9f80a0e197f73a87365c494facc
soulismo <soulwarrior80@gmail.com>
2024-04-23 15:39:03 +0200

    Bientôt fini

--- FrontEnd/JS/login.js
+++ FrontEnd/JS/login.js
@@ -1,5 +1,6 @@
 // Constante de l'URL de base de l'API
 const baseApiUrl = "http://localhost:5678/api/";
+const loginToken = sessionStorage.getItem("token");
 
 //  soumission du formulaire
 document.addEventListener("submit", (e) => {
@@ -11,6 +12,7 @@ document.addEventListener("submit", (e) => {
     email: document.getElementById("email"),
     password: document.getElementById("password"),
   };
+  console.log(form);
 
   // Envoi d'une requête POST à l'endpoint de connexion des utilisateurs de l'API
   fetch(`${baseApiUrl}users/login`, {
@@ -33,11 +35,44 @@ document.addEventListener("submit", (e) => {
     } else {
       // Si la réponse est réussie (code de statut 200), traitement des données JSON de la réponse
       response.json().then((data) => {
-        // Stockage du jeton d'authentification dans sessionStorage
+        // Stockage du token d'authentification dans sessionStorage
         sessionStorage.setItem("token", data.token);
+
         // Redirection vers la page index.html
         window.location.replace("index.html");
       });
     }
   });
 });
+console.log("..");
+const generateLoginButton = () => {
+  console.log("text");
+  const authButton = document.querySelector(".authButton");
+  if (authButton) {
+    authButton.innerHTML = "";
+    const a = document.createElement("a");
+    a.innerHTML = "login";
+    a.href = "login.html";
+    authButton.appendChild(a);
+  }
+};
+const generateLogoutButton = () => {
+  const authButton = document.querySelector(".authButton");
+  if (authButton) {
+    authButton.innerHTML = "";
+    const a = document.createElement("a");
+    a.innerHTML = "logout";
+    const logoutUser = () => {
+      generateLoginButton();
+      sessionStorage.removeItem("token");
+      window.location.replace("index.html");
+    };
+    a.addEventListener("click", logoutUser);
+    authButton.appendChild(a);
+  }
+};
+if (loginToken) {
+  generateLogoutButton();
+} else {
+  generateLoginButton();
+}
